d865d2ff43678f9502cb87cf077cb9f6d25b28b6
Milton <milton.lima@outlook.com>
2023-07-31 10:26:45 -0300

    fix: calc of memory field

--- cmd/nodes.go
+++ cmd/nodes.go
@@ -3,11 +3,16 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"kom/kube"
 	"os"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var nodes *corev1.NodeList
 
 var NodesCmd = &cobra.Command{
 	Use:   "nodes",
@@ -16,19 +21,19 @@ var NodesCmd = &cobra.Command{
 }
 
 func showNodesMetrics(cmd *cobra.Command, args []string) {
-	config, err := getConfig()
+	config, err := kube.GetConfig()
 	if err != nil {
 		fmt.Println("Error to get config", err)
 		os.Exit(1)
 	}
 
-	clientset, err := NewClientSet(config)
+	clientset, err := kube.NewClientSet(config)
 	if err != nil {
 		fmt.Println("Error to create kubernetes client", err)
 		os.Exit(1)
 	}
 
-	metricsClientset, err := NewMetricsClientSet(config)
+	metricsClientset, err := kube.NewMetricsClientSet(config)
 	if err != nil {
 		fmt.Println("Error to list metrics of kubernetes", err)
 		os.Exit(1)
@@ -44,19 +49,22 @@ func showNodesMetrics(cmd *cobra.Command, args []string) {
 
 	for _, node := range nodes.Items {
 		nodeName := node.Name
-		metrics, err := metricsClientset.MetricsV1beta1().NodesMetricses().Get(context.Background(), nodeName, metav1.GetOptions{})
+		metrics, err := metricsClientset.MetricsV1beta1().NodeMetricses().Get(context.Background(), nodeName, metav1.GetOptions{})
 		if err != nil {
 			fmt.Printf("Error to get metrics from node %s: %s\n", nodeName, err)
 			continue
 		}
 
-		cpuUsage := metrics.Usage.CPU.MilliValue() / 10
-		coloredCPU := getColorValue(cpuUsage)
+		cpuUsage := metrics.Usage.Cpu().MilliValue() / 10
+		coloredCPU := getColorValue(int(cpuUsage))
 
-		memoryUsage := metrics.Usage.Memory.Value()
-		coloredMemory := getColorValue(memoryUsage)
+		// memoryUsage := metrics.Usage.Memory().Value() / (1024 * 1024)
+		memoryUsage := float64(metrics.Usage.Memory().Value()) / float64(node.Status.Capacity.Memory().Value()) * 100.0
+		coloredMemory := getColorValue(int(memoryUsage))
 
-		row := []string{nodeName, fmt.Sprintf("%d", coloredCPU), fmt.Sprintf("%d", coloredMemory)}
+		// row := []string{nodeName, fmt.Sprintf("%d", coloredCPU), fmt.Sprintf("%d", coloredMemory)}
+		row := []string{nodeName, coloredCPU, coloredMemory}
+
 		table.Append(row)
 
 	}
