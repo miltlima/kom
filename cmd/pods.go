dfa8de51b23f0e559094ba8bd471de62c7ac0bf6
Milton <milton.lima@outlook.com>
2023-07-31 10:20:16 -0300

    feat: added container show and fix memory field

--- cmd/pods.go
+++ cmd/pods.go
@@ -3,11 +3,16 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"kom/kube"
 	"os"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var pods *corev1.PodList
 
 var PodsCmd = &cobra.Command{
 	Use:   "pods",
@@ -16,54 +21,65 @@ var PodsCmd = &cobra.Command{
 }
 
 func showPodMetrics(cmd *cobra.Command, args []string) {
-	config, err := getConfig()
+	config, err := kube.GetConfig()
 	if err != nil {
 		fmt.Println("Error to get config", err)
 		os.Exit(1)
 	}
 
-	clientset, err := NewClientSet(config)
+	clientset, err := kube.NewClientSet(config)
 	if err != nil {
 		fmt.Println("Error to create kubernetes client", err)
 		os.Exit(1)
 	}
 
-	metricsClientset, err := NewMetricsClientSet(config)
+	metricsClientset, err := kube.NewMetricsClientSet(config)
 	if err != nil {
 		fmt.Println("Error to list metrics of kubernetes", err)
 		os.Exit(1)
 	}
 
-	pods, err = clientset.CoreV1().Pods().List(context.Background(), metav1.ListOptions{})
+	pods, err = clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		fmt.Println("Error to list pods", err)
 		os.Exit(1)
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Container", "CPU Usage %", "Memory Usage %"})
+	table.SetHeader([]string{"Namespace", "Pod", "Container", "CPU Usage %", "Memory Usage %"})
 
 	for _, pod := range pods.Items {
 		podName := pod.Name
 		podNamespace := pod.Namespace
-		metrics, err := metricsClientset.MetricsV1beta1().PodMetricses().Get(podNamespace).Get(context.Background(), podName, metav1.GetOptions{})
+		podMetricsList, err := metricsClientset.MetricsV1beta1().PodMetricses(podNamespace).List(context.Background(), metav1.ListOptions{})
 		if err != nil {
-			fmt.Printf("Error to get metrics from pod %s: %s\n", podNamespace, podName, err)
+			fmt.Printf("Error to get metrics from pod %s%s: %s\n", podNamespace, podName, err)
 			continue
 		}
 
-		for _, container := range metrics.Containers {
-			cpuUsage := container.Usage.Cpu.MilliValue() / 10
-			coloredCPU := getColorValue(cpuUsage)
-
-			memoryUsage := container.Usage.Memory.Value()
-			coloredMemory := getColorValue(memoryUsage)
-
-			row := []string{container.Name, fmt.Sprintf("%d", coloredCPU), fmt.Sprintf("%d", coloredMemory)}
-			table.Append(row)
-
+		nodeName := pod.Spec.NodeName
+		node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			fmt.Printf("Error to get node %s: %s\n", nodeName, err)
+			continue
 		}
 
+		for _, podMetrics := range podMetricsList.Items {
+			for _, container := range podMetrics.Containers {
+				cpuUsage := container.Usage.Cpu().MilliValue() / 10
+				coloredCPU := getColorValue(int(cpuUsage))
+
+				// memoryUsage := container.Usage.Memory().Value() / (1024 * 1024)
+				memoryUsage := float64(container.Usage.Memory().Value()) / float64(node.Status.Capacity.Memory().Value()) * 100.0
+				coloredMemory := getColorValue(int(memoryUsage))
+
+				row := []string{podNamespace, podName, container.Name, coloredCPU, coloredMemory}
+				table.Append(row)
+
+			}
+		}
 	}
+
 	table.Render()
+
 }
